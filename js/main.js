038f76dfdd1c412027ddf2f1c5c50eba4c26be21
Antoine Geboers <antoine@geboersjes.nl>
2016-10-20 16:44:29 +0200

    Initial checkin

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document)
+    .ready(function() {
+        toggle_nav_container();
+        gotoByScroll();
+    });
+
+var toggle_nav_container = function() {
+
+    var $toggleButton = $("#toggle_m_nav");
+    var $navContainer = $("#m_nav_container");
+    var $menuButtonBars = $(".m_nav_ham");
+    var $wrapper = $("#wrapper");
+    var $navItem = $(".m_nav_item a");
+
+    var show = function() {
+        $wrapper.removeClass("closed_wrapper");
+        $wrapper.addClass("open_wrapper");
+        $navContainer.slideDown(200).addClass("container_open").css("z-index", "2");
+        $menuButtonBars.removeClass("button_closed");
+        $menuButtonBars.addClass("button_open");
+        $("#m_ham_1").addClass("m_nav_ham_1_open");
+        $("#m_ham_2").addClass("m_nav_ham_2_open");
+        $("#m_ham_3").addClass("m_nav_ham_3_open");
+    };
+    var hide = function() {
+        $navContainer.css("z-index", "0").removeClass("container_open").slideUp(200);
+        $menuButtonBars.removeClass("button_open");
+        $menuButtonBars.addClass("button_closed");
+        $wrapper.removeClass("open_wrapper");
+        $wrapper.addClass("closed_wrapper");
+        $("#m_ham_1").removeClass("m_nav_ham_1_open");
+        $("#m_ham_2").removeClass("m_nav_ham_2_open");
+        $("#m_ham_3").removeClass("m_nav_ham_3_open");
+    };
+    $navItem.on("click", function() { hide(); });
+
+    // toggle the container on click of button (can be remapped to $menuButton)
+    $toggleButton.on("click",
+        function() {
+            if ($navContainer.is(":hidden")) {
+                show();
+            } else {
+                hide();
+            }
+        });
+};
+
+
+// Function that takes the href value of links in the navbar and then scrolls 
+// the div on the page whose ID matches said value. This only works if you use 
+// a consistent naming scheme for the navbar anchors and div IDs
+var gotoByScroll = function() {
+    $(".m_nav_item a")
+        .on("click",
+            function(e) {
+                e.preventDefault();
+                $("html, body")
+                    .animate({
+                            scrollTop: $($(this).attr("href")).offset().top - 50
+                        },
+                        "slow");
+            });
+};
---
+++ js/responseForm.js
@@ -0,0 +1,65 @@
+﻿(function (ns) {
+    ns.responseForm = function me() {
+        me.fullName = ko.observable();
+        me.phone = ko.observable();
+        me.email = ko.observable();
+        me.subject = ko.observable();
+        me.message = ko.observable();
+
+        me.hasError = ko.observable(false);
+        me.hasResponse = ko.observable(false);
+        me.responseMessage = ko.observable();
+
+        me.btn_click_submit = function () {
+            // get form data
+            var data = JSON.stringify({
+                source: "247up.nl",
+                fullName: me.fullName(),
+                phone: me.phone(),
+                email: me.email(),
+                subject: me.subject(),
+                message: me.message()
+            });
+
+            // reset form
+            me.fullName("");
+            me.phone("");
+            me.email("");
+            me.subject("");
+            me.message("");
+            me.responseMessage("");
+            me.hasError(false);
+            me.hasResponse(false);
+
+            // send message
+            var url = "https://script.google.com/macros/s/AKfycbylBuCkhBdG3jH6H2h3T4cQPnF41X4_DXv275n1J5i75IQ_35Rz/exec";
+            //var url = "https://script.google.com/macros/s/AKfycbz9G0kaTHSeqSAatNdf3LOu_1mqnIK9o_zKz634sTJK/dev";
+            url += "?data=" + encodeURIComponent(data);
+            $.ajaxSetup({ crossOrigin: true });
+            $.ajax({
+                crossDomain: true,
+                url: url,
+                method: "GET",
+                dataType: "jsonp",
+                success: function (args) {
+                    me.hasError(args.hasError);
+                    me.hasResponse(true);
+                    var o = $.parseJSON(args);
+                    me.responseMessage(o.responseMessage);
+                },
+                error: function(args) {
+                    me.hasError(true);
+                    me.hasResponse(true);
+                    me.responseMessage(args.statusText);
+                    console.error(args.statusText);
+                }
+            });
+        }
+
+        return me;
+    }();
+  
+    $(document).ready(function() {
+        ko.applyBindings(ns.responseForm, $("#contactForm")[0]);
+    });
+})(this);
