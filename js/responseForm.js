ea0014e8f8e4c48a091635e72c21de1ccefc6412
Antoine Geboers <antoine@geboersjes.nl>
2016-10-30 12:56:40 +0100

    form validatie

--- js/responseForm.js
+++ js/responseForm.js
@@ -1,26 +1,81 @@
 ﻿(function(ns) {
     ns.responseForm = function me() {
         me.fullName = ko.observable();
+        me.fullNameValid = ko.observable(true);
         me.phone = ko.observable();
+        me.phoneValid = ko.observable(true);
         me.email = ko.observable();
+        me.emailValid = ko.observable(true);
         me.subject = ko.observable();
+        me.subjectValid = ko.observable(true);
         me.message = ko.observable();
+        me.messageValid = ko.observable(true);
 
         me.hasError = ko.observable(false);
         me.hasResponse = ko.observable(false);
         me.responseMessage = ko.observable();
 
-        me.btn_click_submit = function() {
+        me.btn_click_submit = function () {
+            if (me.validateForm()) {
+                me.sendForm();
+            }
+        };
+        me.validateForm = function () {
+            var isValid = true;
+
+            // fullName
+            me.fullNameValid(true);
+            if (!me.fullName()) {
+                isValid = false;
+                me.fullNameValid(false);
+            }
+
+            // phone
+            me.phoneValid(true);
+            if (!me.phone()) {
+                isValid = false;
+                me.phoneValid(false);
+            }
+
+            // email
+            me.emailValid(true);
+            if (!me.email()) {
+                isValid = false;
+                me.emailValid(false);
+            }
+
+            // subject
+            me.subjectValid(true);
+            if (!me.subject()) {
+                isValid = false;
+                me.subjectValid(false);
+            }
+
+            // message
+            me.messageValid(true);
+            if (!me.message()) {
+                isValid = false;
+                me.messageValid(false);
+            }
+
+            return isValid;
+        };
+        me.sendForm = function() {
             // reset form
             me.hasError(false);
             me.hasResponse(false);
 
             // send message
+            $.blockUI({
+                css: {
+                    border: 'none', backgroundColor: '#00C6FF', color: '#fff'
+                }
+            });
             $.ajax({
                 url: me.createUrl(),
                 method: "GET",
                 dataType: "jsonp",
-                success: function(args) {
+                success: function (args) {
                     me.hasResponse(true);
                     me.hasError(args.hasError);
                     me.responseMessage(args.responseMessage);
@@ -30,12 +85,14 @@
                         console.log(args);
                         me.clearForm();
                     }
+                    $.unblockUI();
                 },
-                error: function(args) {
+                error: function (args) {
                     me.hasResponse(true);
                     me.hasError(true);
                     me.responseMessage(args.statusText);
                     console.error(args.statusText);
+                    $.unblockUI();
                 }
             });
         };
